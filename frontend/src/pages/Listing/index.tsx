35202220ea5e445d75e60a5fec2b2e8046113aa5
Telmo Ricardo <telmoricardorosa@gmail.com>
2022-03-09 22:54:16 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,20 @@
+import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 
 function Listing(){
     
     return(
-        <Pagination></Pagination>
+       <>
+            <Pagination />
+
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                    <MovieCard />
+                    </div>
+                </div>
+            </div>
+       </>
     );
 }
 
